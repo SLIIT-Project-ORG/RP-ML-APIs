cb9718744292a04a6e37fb95f4e6f788e7617d39
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2023-08-18 10:04:15 +0530

    RP ML APIs

--- index.py
+++ index.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # from routes.test import test
 # from routes.DiseaseIdentify import diseaseIdentify
 from routes.TextIdentify import textIdentify
 
 app = FastAPI()
 
+origins = [
+    "http://127.0.0.1:8000",
+    "http://localhost:19006"
+]
+
+# You can customize other CORS settings as well, like methods, headers, etc.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],   # You can specify specific HTTP methods here
+    allow_headers=["*"],   # You can specify specific HTTP headers here
+)
+
 # app.include_router(test,prefix="/test")
 # app.include_router(diseaseIdentify,prefix="/disease-identify")
 app.include_router(textIdentify,prefix="/text-identify")
+
+# Run the FastAPI server using Uvicorn
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- routes/TextIdentify.py
+++ routes/TextIdentify.py
@@ -1,14 +1,15 @@
-import os
-
-from fastapi import APIRouter, FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
-from tensorflow.keras.models import load_model
-from sklearn.preprocessing import LabelEncoder
+import base64
 import cv2
 import numpy as np
-import tempfile
-import shutil
 import pandas as pd
+import tempfile
+import pickle
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.models import load_model
+from fastapi import APIRouter, FastAPI, Form
+from fastapi.responses import JSONResponse
+from io import BytesIO
+from PIL import Image
 
 # Load the trained model
 model = load_model('pickle\\handwrite_model.h5')
@@ -20,9 +21,6 @@ img_size_w = 50
 img_size_h = 20
 
 # Load the label encoder
-# (Make sure to adjust the path if necessary)
-import pickle
-
 with open(label_encoder_path, 'rb') as f:
     label_encoder = pickle.load(f)
 
@@ -35,17 +33,16 @@ temp_dir = tempfile.mkdtemp()
 
 textIdentify = APIRouter()
 
-# Route to handle image uploads and make predictions
-@textIdentify.post("/predict/")
-async def predict_image(file: UploadFile = File(...)):
+# Route to handle base64-encoded image and make predictions
+@textIdentify.post("/predict")
+async def predict_base64_image(file: str = Form(...)):
     try:
-        # Save the uploaded image to the temporary directory
-        file_path = f"{temp_dir}/{file.filename}"
-        with open(file_path, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
+        # Decode the base64 image
+        image_data = BytesIO(base64.b64decode(file))
+        img = Image.open(image_data).convert("L")  # Convert to grayscale
 
         # Preprocess the image
-        img_array = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+        img_array = np.array(img)
         new_array = cv2.resize(img_array, (img_size_w, img_size_h))
         array = new_array.reshape(-1, img_size_w, img_size_h, 1)
 
@@ -57,41 +54,20 @@ async def predict_image(file: UploadFile = File(...)):
 
         # Get the corresponding class label from the label encoder
         predicted_class = label_encoder.inverse_transform([y])[0]
-        print("pred ", predicted_class)
-        # Clean up: delete the temporary file
-        os.remove(file_path)
 
         result_list = predicted_class.split(",")
-        capital_result_list = []
-        for i in result_list:
-            capital_result_list.append(i.upper())
+        capital_result_list = [i.upper() for i in result_list]
 
         # Set dictionary values to predicted result
         df = pd.read_excel(dictionary_path)
+        data_dictionary = {row["ID"]: row["LABEL"] for index, row in df.iterrows()}
 
-        # print(df.head())
-        data_dictionary = {}
-        for index, row in df.iterrows():
-            id_value = row["ID"]
-            label_value = row["LABEL"]
-            data_dictionary[id_value] = label_value
+        result_list = [data_dictionary[x] for x in capital_result_list if x in data_dictionary]
 
-        result_list = []
-        # print("Dictionary : ", data_dictionary)
-        key_list = data_dictionary.keys()
-        for x in capital_result_list:
-            if key_list.__contains__(x):
-                result_list.append(data_dictionary.get(x))
-
-        print("ID List : ", capital_result_list)
         return JSONResponse(content={"predicted_class": result_list})
     except Exception as e:
-        print(str(e))
         return JSONResponse(content={"error": str(e)})
 
+app = FastAPI()
 
-# Run the FastAPI server using Uvicorn
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(textIdentify, host="127.0.0.1", port=8000)
+app.include_router(textIdentify, tags=["Image Prediction"])
